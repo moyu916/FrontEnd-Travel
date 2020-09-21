79592a88cd5481a0c2fe1dec550360620e36476a
moyulqy <972413091@qq.com>
2020-09-21 20:23:24 +0800

    digui components

---
+++ src/pages/detail/components/List.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="list">
+        <div class="item" v-for="(item,index) of list" :key="index">
+            <div class="item-title border-bottom">
+                <span>*</span>
+                {{item.title}}
+            </div>
+            <div v-if="item.children" class="item-children">
+                <detail-list :list="item.children"></detail-list>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailList',
+  props: {
+      list: Array
+  }
+}
+</script>
+
+<style scoped>
+    .item-title {
+        line-height: .8rem;
+        font-size: .32rem;
+        padding-left: .2rem;
+    }
+    .item-children {
+        padding: 0 .2rem;
+    }
+</style>
